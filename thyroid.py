7382ec960c8385ea30fc2ef673664e2f1efe2dbd
jeevak25 <98375591+jeevak25@users.noreply.github.com>
2024-05-24 01:02:26 +0530

    Update thyroid.py

--- thyroid.py
+++ thyroid.py
@@ -21,8 +21,8 @@ title_css = f"<h1 style='text-align: center; color: {title_color};'>Thyroid Diag
 # Detect button color
 detect_button_color = '#F63366'
 
-
-st.page_link("test.py", label="Lab Test")
+st.page_link("thyroid.py", label="Home")
+st.page_link("pages/test.py", label="Lab Test")
 
 # Function to preprocess inputs before prediction
 def preprocess_inputs(age, sex, on_thyroxine, query_on_thyroxine, on_antithyroid_meds, sick, pregnant,
